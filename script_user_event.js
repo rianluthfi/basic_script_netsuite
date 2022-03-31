ff18404d3c61d76a35a5d55c6fa6438e4c0542b7
rianluthfi <luthfirian@gmail.com>
2022-03-31 13:15:34 +0700

    Add files via upload

---
+++ script_user_event.js
@@ -0,0 +1,57 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+
+define(['N/record', 'N/ui/serverWidget', 'N/runtime'],
+
+function(record, serverWidget, runtime) {
+   
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {string} scriptContext.type - Trigger type
+     * @param {Form} scriptContext.form - Current form
+     * @Since 2015.2
+     */
+    function beforeLoad(context) {
+
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function beforeSubmit(context) {
+
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function afterSubmit(context) {
+		
+	}
+	
+	
+    return {
+        // beforeLoad: beforeLoad,
+        // beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    };
+    
+});
